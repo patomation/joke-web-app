85ad93c8e1c59edbf586bf653d12976c816c783c
patomation <patomation@gmail.com>
2019-07-16 23:04:30 +0700

    sets up object classes styles

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
       lastName: 'Norris',
       results: 10,
       random: true,
-      jokes: [],
+      jokes: [{joke: 'Human cloning is outlawed because of Chuck Norris, because then it would be possible for a Chuck Norris roundhouse kick to meet another Chuck Norris roundhouse kick. Physicists theorize that this contact would end the universe.'}],
     };
   }
 
@@ -35,8 +35,13 @@ export default class App extends React.Component {
     let listItems = [];
     for (var i = 0; i < this.state.jokes.length; i++) {
       listItems.push(
-        <li className="o-list-item" key={`joke-${i}`}>
-          {this.state.jokes[i].joke}
+        <li className="o-list-item row" key={`joke-${i}`}>
+          <div className="col-11">
+            {this.state.jokes[i].joke}
+          </div>
+          <div className="col-1">
+            <button className="o-button">like</button>
+            </div>
         </li>
       );
     }
@@ -50,14 +55,16 @@ export default class App extends React.Component {
   render(){
 
     return(
-      <div className="c-app">
-          <header className="c-header">
-            <input className="o-input" onChange={(e)=>{this.setState({firstName:e.target.value})}} value={this.state.firstName || ''}></input>
-            <input className="o-input" onChange={(e)=>{this.setState({lastName:e.target.value})}} value={this.state.lastName || ''}></input>
-            <input className="o-input" onChange={(e)=>{this.setState({results:e.target.value})}} value={this.state.results || 0} type='number'></input>
-            <button className="o-button" onClick={this._getJokes.bind(this)}>Get Jokes</button>
+      <div className="c-app container">
+          <header className="c-header  row">
+            <h1 className="o-h1"> {this.state.firstName} {this.state.lastName} Joke Generator</h1>
+
+            <input className="o-input col-5" onChange={(e)=>{this.setState({firstName:e.target.value})}} value={this.state.firstName || ''}></input>
+            <input className="o-input col-5" onChange={(e)=>{this.setState({lastName:e.target.value})}} value={this.state.lastName || ''}></input>
+            <input className="o-input col-2" onChange={(e)=>{this.setState({results:e.target.value})}} value={this.state.results || 0} type='number'></input>
+            <button className="o-button col-12" onClick={this._getJokes.bind(this)}>Get Jokes</button>
           </header>
-          <main>
+          <main className="o-main">
             { this._renderList() }
           </main>
       </div>
