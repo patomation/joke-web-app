8b604e0d834dfe4a63af2cf31c029eee75ecf1d4
patomation <patomation@gmail.com>
2019-07-16 17:55:41 +0700

    react boilerplate added from https://github.com/patomation/react-boilerplate

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import style from "./sass/main.scss";
+
+const Index = () => {
+  return (
+    <div>
+      When Chuck Norris does a pushup he is infact pushing the earth away from him.
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
